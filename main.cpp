7b62d729cad94d54ba0b787e816d4987b0e4a546
Keith <KD.Chambers97@Gmail.com>
2017-04-20 13:24:03 +0100

    Connected QML and C++ Backend in main

--- main.cpp
+++ main.cpp
@@ -1,12 +1,20 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
+#include "guicontrollayer.h"
+#include "util.h"
+
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
+	
+	GuiControlLayer guiControlLayer(Util::readJson(QString("C:/libs/client_info.json")), &app);
+
+    engine.rootContext()->setContextProperty("BackEnd", &guiControlLayer);
+	
     engine.load(QUrl(QLatin1String("qrc:/main.qml")));
 
     return app.exec();
