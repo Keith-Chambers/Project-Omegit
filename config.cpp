8e5e7d5572ec8248bc343b9b2f887b71beaf4982
Keith <KD.Chambers97@Gmail.com>
2017-04-23 23:44:18 +0200

    Added two more entry points

--- config.cpp
+++ config.cpp
@@ -2,5 +2,6 @@
 
 namespace config {
     QUrl apiUrl {"https://ditalk.xyz"};
-    QHash<QString, QString> endPoints({{"login", "/login.php"}, {"logout", "/logout.php"}});
+    QHash<QString, QString> endPoints(  {{"login", "/login.php"}, {"logout", "/logout.php"},
+                                        {"chat", "/chat.php"}, {"usersConnect", "/usersConnect.php"}});
 }
